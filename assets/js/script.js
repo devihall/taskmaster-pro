1364af42827b55f770727e0ce467309e5d784157
devihall <devis0306@gmail.com>
2022-07-10 15:06:50 -0400

    drag and drop feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -188,3 +188,24 @@ $("#remove-tasks").on("click", function() {
 loadTasks();
 
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function (event) {
+    console.log("activate", this);
+  },
+  deactivate: function (event) {
+    console.log("deactivate", this);
+  },
+  over: function (event) {
+    console.log("over", event.target);
+  },
+  out: function (event) {
+    console.log("out", event.target);
+  },
+  update: function (event) {
+    console.log($(this).children());
+  },
+});
